1d17174ab65c28737dcfae8c2f5432a82458af59
ENG-Loay <essamloay2@gmail.com>
2024-03-14 18:05:40 +0200

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,57 +14,38 @@ void  playsound(int soundNumber)
   final player = AudioPlayer();
   player.play(AssetSource('audio/note$soundNumber.wav'));
 }
+  Expanded buildkey({Color color = Colors.white, int soundnumber = 0})
+  {
+  return Expanded(
+    child: MaterialButton(
+
+      color: color,
+      onPressed: () {
+        playsound(soundnumber);
+      },
+    ),
+  );
+}
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
             child:Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                MaterialButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    playsound(1);
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.orange,
-                  onPressed: () {
-                    playsound(2);
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.yellow,
-                  onPressed: () {
-                    playsound(3);
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    playsound(4);
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.teal,
-                  onPressed: () {
-                    playsound(5);
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.blue,
-                  onPressed: () {
-                    playsound(6);
-                  },
-                ),
-                MaterialButton(
-                  color: Colors.purple,
-                  onPressed: () {
-                    playsound(7);
-                  },
-                ),
-              ],
+                buildkey(color:Colors.red,soundnumber:1),
+                buildkey(color:Colors.orange,soundnumber:2),
+                buildkey(color:Colors.yellow,soundnumber:3),
+                buildkey(color:Colors.green,soundnumber:4),
+                buildkey(color:Colors.teal,soundnumber:5),
+                buildkey(color:Colors.blue,soundnumber:6),
+                buildkey(color:Colors.purple,soundnumber:7),
+
+
+        ],
             ),
 
 
